1026ca46aa5c96bc7846bd7e3ed1fa934e9453ab
Bingo Bobo <matmazur90@gmail.com>
2018-09-19 19:17:36 +0200

    created Student class as model entity

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,90 @@
+package model;
+
+import com.sun.istack.internal.NotNull;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+
+    @Id
+    @GeneratedValue
+    @Column("id")
+    private String id;
+
+    @Column("first_name")
+    @NotNull
+    private String first_name;
+
+    @Column("last_name")
+    @NotNull
+    private String last_name;
+
+    @Column("email")
+    @NotNull
+    private String email;
+
+    public Student(String first_name, String last_name, String email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+    }
+
+    public Student() {
+    }
+
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) &&
+                Objects.equals(first_name, student.first_name) &&
+                Objects.equals(last_name, student.last_name) &&
+                Objects.equals(email, student.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, first_name, last_name, email);
+    }
+}
+
